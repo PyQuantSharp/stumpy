c576f4e167503d193382578b3f64fd9e06d129ea
Sean Law <seanmylaw@gmail.com>
2025-09-02 10:08:17 -0400

    Updated ray python version id logic

--- ray_python_version.py
+++ ray_python_version.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python
 
-import requests
+import pandas as pd
 from packaging.version import Version
 
-classifiers = (
-    requests.get("https://pypi.org/pypi/ray/json").json().get("info").get("classifiers")
-)
-
-versions = []
-for c in classifiers:
-    x = c.split()
-    if "Python" in x:
-        versions.append(x[-1])
+os_map = {"linux": 1, "macos": 2, "windows": 3}
+versions = None
+for os, table_num in os_map.items():
+    df = pd.read_html("https://docs.ray.io/en/latest/ray-overview/installation.html")[
+        table_num
+    ]
+    mask = df.apply(lambda col: col.astype(str).str.contains("beta")).any(axis=1)
+    df = df.loc[~mask]
+    for col in df.columns:
+        if versions is None:
+            versions = set(
+                df[col].astype(str).str.extract(r"(\d+\.\d+)").values.flatten().tolist()
+            )
+        else:
+            versions.intersection(
+                (
+                    set(
+                        df[col]
+                        .astype(str)
+                        .str.extract(r"(\d+\.\d+)")
+                        .values.flatten()
+                        .tolist()
+                    )
+                )
+            )
+versions = list(versions)
 
 versions.sort(key=Version)
 print(versions[-1])
