ed02f7aa98a53252b26f2c4270cab43309e93ff9
Sean Law <seanmylaw@gmail.com>
2025-11-09 06:55:13 -0500

    Fixed long string failing flake8

--- min_versions.py
+++ min_versions.py
@@ -9,7 +9,9 @@ from packaging.version import Version
 
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/91.0.4472.124 Safari/537.36"
 }
 
 
