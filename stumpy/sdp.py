2d8854a00afca572b00758f4abfcc26739396e43
Nima Sarajpoor <nimasarajpoor@gmail.com>
2026-02-12 23:43:24 -0500

    Add new sdp functions (#1128)
    * improve njit_sdp
    
    * simplify function
    
    * add new sdp func and its test
    
    * add pocketfft sdp and test
    
    * fixed missing imports
    
    * fixed coverage
    
    * refactored test functions
    
    * minor change
    
    * enhanced test cases
    
    * add minor comment
    
    * Minor change

--- stumpy/sdp.py
+++ stumpy/sdp.py
@@ -1,6 +1,8 @@
 import numpy as np
 from numba import njit
-from scipy.signal import convolve
+from scipy.fft import next_fast_len
+from scipy.fft._pocketfft.basic import c2r, r2c
+from scipy.signal import convolve, oaconvolve
 
 from . import config
 
@@ -23,11 +25,14 @@ def _njit_sliding_dot_product(Q, T):
     out : numpy.ndarray
         Sliding dot product between `Q` and `T`.
     """
-    m = Q.shape[0]
-    l = T.shape[0] - m + 1
+    m = len(Q)
+    l = len(T) - m + 1
     out = np.empty(l)
     for i in range(l):
-        out[i] = np.dot(Q, T[i : i + m])
+        result = 0.0
+        for j in range(m):
+            result += Q[j] * T[i + j]
+        out[i] = result
 
     return out
 
@@ -49,12 +54,59 @@ def _convolve_sliding_dot_product(Q, T):
     output : numpy.ndarray
         Sliding dot product between `Q` and `T`.
     """
-    n = T.shape[0]
-    m = Q.shape[0]
-    Qr = np.flipud(Q)  # Reverse/flip Q
-    QT = convolve(Qr, T)
+    return convolve(np.flipud(Q), T, mode="valid")
 
-    return QT.real[m - 1 : n]
+
+def _oaconvolve_sliding_dot_product(Q, T):
+    """
+    Use scipy's oaconvolve to calculate the sliding dot product.
+
+    Parameters
+    ----------
+    Q : numpy.ndarray
+        Query array or subsequence
+
+    T : numpy.ndarray
+        Time series or sequence
+
+    Returns
+    -------
+    output : numpy.ndarray
+        Sliding dot product between `Q` and `T`.
+    """
+    return oaconvolve(np.ascontiguousarray(Q[::-1]), T, mode="valid")
+
+
+def _pocketfft_sliding_dot_product(Q, T):
+    """
+    Use scipy.fft._pocketfft to compute
+    the sliding dot product.
+
+    Parameters
+    ----------
+    Q : numpy.ndarray
+        Query array or subsequence
+
+    T : numpy.ndarray
+        Time series or sequence
+
+    Returns
+    -------
+    output : numpy.ndarray
+        Sliding dot product between `Q` and `T`.
+    """
+    n = len(T)
+    m = len(Q)
+    next_fast_n = next_fast_len(n, real=True)
+
+    tmp = np.empty((2, next_fast_n))
+    tmp[0, :m] = Q[::-1]
+    tmp[0, m:] = 0.0
+    tmp[1, :n] = T
+    tmp[1, n:] = 0.0
+    fft_2d = r2c(True, tmp, axis=-1)
+
+    return c2r(False, np.multiply(fft_2d[0], fft_2d[1]), n=next_fast_n)[m - 1 : n]
 
 
 def _sliding_dot_product(Q, T):
